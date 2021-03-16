8e768cabe01038ac099a22e0e1353b25c57ce219
Tinkergit <nicholas.stagno@gmail.com>
2021-03-16 18:09:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :index
+    end
+
+    configure do
+        enable :sessions
+        set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64)}
+    end
+
+    post '/checkout' do
+        item=params[:item]
+        session[:item]=item
+        @session = session
+    end
+     
+
 end
